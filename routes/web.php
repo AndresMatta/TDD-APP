38bc8fa676d16bb03ea0de86d4c129bb752c3b2c
Andres Matta Morales <andres.sm123@gmail.com>
2018-12-27 02:28:02 -0600

    Initial Setup / Create & View Projects.

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,17 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::get('/projects/{project}', 'ProjectsController@show');
+
+Route::post('/projects', 'ProjectsController@store')->middleware('auth');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
